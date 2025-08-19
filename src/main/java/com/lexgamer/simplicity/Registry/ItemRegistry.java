cc8a3bac3b4d531841a6bc0ed53d76a954c61502
Autovw <68228006+Autovw@users.noreply.github.com>
2025-08-19 18:54:08 +0200

    Register ore block items and run data generators

--- src/main/java/com/lexgamer/simplicity/Registry/ItemRegistry.java
+++ src/main/java/com/lexgamer/simplicity/Registry/ItemRegistry.java
@@ -11,6 +11,8 @@ public class ItemRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Simplicity.MODID);
 
     public static final DeferredItem<BlockItem> RUBY_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("ruby_block", BlockRegistry.RUBY_BLOCK);
+    public static final DeferredItem<BlockItem> RUBY_ORE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("ruby_ore", BlockRegistry.RUBY_ORE);
+    public static final DeferredItem<BlockItem> DEEPSLATE_RUBY_ORE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("deepslate_ruby_ore", BlockRegistry.DEEPSLATE_RUBY_ORE);
     public static final DeferredItem<Item> RUBY = ITEMS.registerItem("ruby", Item::new);
 
 }
